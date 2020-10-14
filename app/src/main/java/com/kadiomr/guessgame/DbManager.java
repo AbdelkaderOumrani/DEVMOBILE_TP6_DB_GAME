7e644a3f39f63596f644fd0e71b88775deb51b42
AbdelkaderOumrani <47723408+AbdelkaderOumrani@users.noreply.github.com>
2020-10-15 00:53:41 +0100

    Create Db helper

--- app/src/main/java/com/kadiomr/guessgame/DbManager.java
+++ app/src/main/java/com/kadiomr/guessgame/DbManager.java
@@ -1,4 +1,76 @@
 package com.kadiomr.guessgame;
 
-public class DbManager {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DbManager extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "WordsDatabase";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "words";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_WORD = "word";
+    private static final String COLUMN_ANSWERED = "answered";
+
+    DbManager(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sql = "CREATE TABLE " + TABLE_NAME + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COLUMN_WORD + " varchar(200) NOT NULL," +
+                COLUMN_ANSWERED + " INTEGER NOT NULL);";
+
+                sqLiteDatabase.execSQL(sql);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    boolean addWord(Word word){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_WORD, word.getWord());
+        contentValues.put(COLUMN_ANSWERED, word.getAnswered());
+        return db.insert(TABLE_NAME,null,contentValues)!=-1;
+    }
+
+    void wipeAllWords(){
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("delete from "+ TABLE_NAME);
+    }
+    List<Word> getAllWords() {
+        List<Word> words = new ArrayList<>();
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        if(cursor.moveToFirst()){
+            do{
+                Word w = new Word();
+                w.setId(cursor.getInt(cursor.getColumnIndex(COLUMN_ID)));
+                w.setWord(cursor.getString(cursor.getColumnIndex(COLUMN_WORD)));
+                w.setAnswered(cursor.getInt(cursor.getColumnIndex(COLUMN_ANSWERED)));
+                words.add(w);
+            }
+            while (cursor.moveToNext());
+        }
+        return words;
+    }
+
+    boolean setAnswered(long rowId) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_ANSWERED, 1);
+        return db.update(TABLE_NAME, contentValues, COLUMN_ID+"="+rowId, null)!=-1;
+    }
+
+
 }
--- app/src/main/java/com/kadiomr/guessgame/Word.java
+++ app/src/main/java/com/kadiomr/guessgame/Word.java
@@ -1,4 +1,43 @@
 package com.kadiomr.guessgame;
 
 public class Word {
+
+    long id;
+    String word;
+    int answered;
+
+    public Word(long id, String word, int answered) {
+        this.id = id;
+        this.word = word;
+        this.answered = answered;}
+
+    public Word(String word) {
+        this.word = word;
+        this.answered = 0;}
+
+    public Word() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getAnswered() {
+        return answered;
+    }
+
+    public void setAnswered(int answered) {
+        this.answered = answered;
+    }
 }
